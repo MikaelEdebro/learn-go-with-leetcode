05f42c2fc59fb322837ca117090378b3b9402abc
Mikael Edebro <mikael.edebro@polestar.com>
2022-06-14 22:26:32 +0200

    Test pipeline with failing test

--- 25_search_insert_position_test.go
+++ 25_search_insert_position_test.go
@@ -14,7 +14,7 @@ func TestSearchInsert(t *testing.T) {
 	inputs := make([]Search, 0)
 	inputs = append(inputs, Search{inputs: []int{1, 3, 5, 6}, target: 5, expectedIndex: 2})
 	inputs = append(inputs, Search{inputs: []int{1, 3, 5, 6}, target: 2, expectedIndex: 1})
-	inputs = append(inputs, Search{inputs: []int{1, 3, 5, 6}, target: 7, expectedIndex: 4})
+	inputs = append(inputs, Search{inputs: []int{1, 3, 5, 6}, target: 7, expectedIndex: 5})
 
 	for _, s := range inputs {
 		if searchInsert(s.inputs, s.target) != s.expectedIndex {
